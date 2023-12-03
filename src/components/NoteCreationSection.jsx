dc810b92b597548dbc81f3e01661e5935d0bc213
Roshan <sroshanahmad@gmail.com>
2023-12-03 20:05:29 +1300

    changes to CSS

--- src/components/NoteCreationSection.jsx
+++ src/components/NoteCreationSection.jsx
@@ -13,8 +13,14 @@ export function NoteCreationSection({trigerToggleUpdate}) {
     return (
         <div className="note-creation-section">
             <h2>Create a Note</h2>
-              <textarea name="note" id="note" placeholder="Write your note here..." onChange={ (e) => setNote(e.target.value)}></textarea>
-              <button id="save-button" onClick={ () => sendNoteData(data)}>Save Note</button>
+            <div id="notes-container">
+                <div>
+                    <textarea name="note" id="note" placeholder="Write your note here..." onChange={ (e) => setNote(e.target.value)}></textarea>
+                </div>
+                <div>
+                <button id="save-button" onClick={ () => sendNoteData(data)}>Save Note</button>
+                </div>
+            </div>
         </div>
     )
 }
