5b3deba130faa44e7956617e6e623febef80c82f
Roshan <sroshanahmad@gmail.com>
2023-12-04 23:17:30 +1300

    error handling

--- src/api/APICalls.jsx
+++ src/api/APICalls.jsx
@@ -1,43 +1,71 @@
-const baseURL = 'http://127.0.0.1:5000'
+const baseURL = 'http://127.0.0.1:5000/api'
 
 export const get = async () => {
-    const response = await fetch(`${baseURL}/notes/`)
-    const data = await response.json()
-    return data
+    try {
+
+        const response = await fetch(`${baseURL}/notes`)
+        const data = await response.json()
+        if(response.ok) return data
+        else throw new Error(`${response.status}: ${response.statusText}`)
+    }
+    catch (error) {
+        console.log(error)
+    }
 }
 
 
 export const post = async (data) => {
+    try{
+        const response = await fetch(`${baseURL}/notes`, {
+            method: "POST",
+            body: JSON.stringify({
+                'data': data,
+              }),
+            headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+        if(response.ok) return 'Note created !'
+        else throw new Error(`${response.status}: ${response.statusText}`)
+    }
+    catch (error) {
+        console.log(error)
+    }
 
-    const response = await fetch(`${baseURL}/notes/`, {
-        method: "POST",
-        body: JSON.stringify({
-            'data': data,
-          }),
-        headers: {
-                "Content-type": "application/json; charset=UTF-8"
-        }
-    })
-    if(response.ok) return 'Note created !'
 }
 
 
 export const put = async (id,data) => {
-    const response = await fetch(`${baseURL}/notes/${id}`, {
-        method: "PUT",
-        body: JSON.stringify({
-            'data': data,
-          }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    })
-    if(response.ok) return 'Note updated !'
+
+    try {
+        const response = await fetch(`${baseURL}/notes/${id}`, {
+            method: "PUT",
+            body: JSON.stringify({
+                'data': data,
+              }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+        if(response.ok) return 'Note updated !'
+        else throw new Error(`${response.status}: ${response.statusText}`)
+    }
+
+    catch (error) {
+        console.log(error)
+    }
 }
 
 
 export const deletes = async (id) => {
-    const response = await fetch(`${baseURL}/notes/${id}`, {
-        method: 'DELETE'})
-    if(response.ok) return 'Note Deleted !'
+    try {
+
+        const response = await fetch(`${baseURL}/notes/${id}`, {
+            method: 'DELETE'})
+        if(response.ok) return 'Note Deleted !'
+        else throw new Error(`${response.status}: ${response.statusText}`)
+    }
+    catch (error){
+        console.log(error)
+    }
 }
